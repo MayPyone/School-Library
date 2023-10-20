0811f393d9bdb3e331aa839da5a1967d5f38f29a
May Pyone <maypyone015@gmail.com>
2023-10-21 01:05:10 +0630

    Update student and person

--- person.rb
+++ person.rb
@@ -1,4 +1,5 @@
 require_relative 'nameable'
+require_relative 'rental'
 class Person < Nameable
   def initialize(age, name: 'unknown', parent_permission: true)
     super()
@@ -6,9 +7,10 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rental = []
   end
 
-  attr_accessor :name, :age
+  attr_accessor :name, :age, :rental
   attr_reader :id
 
   def can_use_services?
@@ -23,5 +25,10 @@ class Person < Nameable
 
   def of_age?
     @age >= 18
+  end
+
+  def add_rental(date)
+    rental = Rental.new(date, self)
+    @rental.push(rental)
   end
 end
--- test.rb
+++ test.rb
@@ -1,10 +1,11 @@
 require_relative 'decorator'
-require_relative 'person'
 require_relative 'capitalize'
 require_relative 'trimmer'
-person = Person.new(22, name: 'maximilianus')
-person.correct_name
-capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-capitalized_trimmed_person = TrimmerDecorator.new(person)
-puts capitalized_trimmed_person.correct_name
+require_relative 'person'
+require_relative 'student'
+# person = Person.new(22, name: 'maximilianus')
+# person.correct_name
+# capitalized_person = CapitalizeDecorator.new(person)
+# puts capitalized_person.correct_name
+# capitalized_trimmed_person = TrimmerDecorator.new(person)
+# puts capitalized_trimmed_person.correct_name
