cb12eac42a4fc4eabcb30b297956665c73398b3b
May Pyone <maypyone015@gmail.com>
2023-10-21 04:41:43 +0630

    Create a book and show the list of book

--- main.rb
+++ main.rb
@@ -1,6 +1,7 @@
 require_relative 'person'
 require_relative 'teacher'
 require_relative 'student'
+require_relative 'book'
 class Main
   def initialize()
     @person = []
@@ -49,12 +50,33 @@ class Main
     end
   end
 
+  def create_book()
+    print 'Title: '
+    title = gets.chomp
+    print 'Author: '
+    author = gets.chomp
+    book = Book.new(title, author)
+    @books.push(book)
+    puts 'Book has been created successfully'
+  end
+
+  def book_list()
+    length = @books.length
+    @books.map do |book|
+      puts "Title: #{book.title}"
+      puts "Author: #{book.author}"
+    end
+    return unless length.zero?
+
+    puts 'No book has been created yet'
+  end
+
   def person_list()
     puts 'No account has created yet' if @person.empty?
     @person.map { |person| puts "[#{person.role}] Name: #{person.name} ID: #{person.id} AGE: #{person.age}" }
   end
 
-  def main()
+  def option()
     puts 'Please choose an option by entering number'
     puts '1 - List all books'
     puts '2 - List all people'
@@ -63,12 +85,20 @@ class Main
     puts '5 - Create a rental'
     puts '6 - List all rentals for a given id'
     puts '7 - Exit'
+  end
+
+  def main()
+    option
     choose = gets.chomp.to_i
     case choose
+    when 1
+      book_list
     when 2
       personList
     when 3
       create_person
+    when 4
+      create_book
     when 7
       return
     else
